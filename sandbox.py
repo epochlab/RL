29e32ef1939343ac6b2ba8bfdc2d4d144c575325
James Roberts <JRoberts86@gmail.com>
2021-07-28 01:00:43 +0100

    Add DOOM

--- sandbox.py
+++ sandbox.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-import gym
+import random, time
 import numpy as np
+
+import gym
+import vizdoom
 
 def atari_search():
     env = gym.make("PongNoFrameskip-v4")
@@ -10,3 +13,23 @@ def atari_search():
         env.render()
         action = np.random.choice(env.action_space.n)
         state_next, reward, terminal, info = env.step(action)
+    env.close()
+
+def doom_search():
+    env = vizdoom.DoomGame()
+    env.load_config('/mnt/vanguard/git/ViZDoom-master/scenarios/basic.cfg')
+    env.init()
+
+    shoot = [0, 0, 1]
+    left = [1, 0, 0]
+    right = [0, 1, 0]
+    action_space = [shoot, left, right]
+
+    env.new_episode()
+    while not env.is_episode_finished():
+        state = env.get_state()
+        img = state.screen_buffer
+        misc = state.game_variables
+        reward = env.make_action(random.choice(action_space))
+        time.sleep(0.02)
+    env.close()
