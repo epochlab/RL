4c5e4189b57f28eea14f5c9510376804586596a9
James Roberts <JRoberts86@gmail.com>
2021-08-19 22:38:24 +0100

    Add actor critic models

--- networks.py
+++ networks.py
@@ -4,6 +4,8 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import backend as K
 
+# DQN -------------------------
+
 def dqn(input_shape, window_length, action_space):
     inputs = layers.Input(shape=(input_shape[0], input_shape[1], window_length,))
 
@@ -34,4 +36,62 @@ def dueling_dqn(input_shape, window_length, action_space):
     adv = layers.Lambda(lambda a: a[:, :] - K.mean(a[:, :], keepdims=True), output_shape=(action_space,))(adv)
 
     action = layers.Add()([value, adv])
+    return keras.Model(inputs=inputs, outputs=action)
+
+# A2C -------------------------
+
+def actor_network(input_shape, action_space):
+    inputs = layers.Input(shape=(input_shape))
+
+    layer1 = layers.Conv2D(32, 8, strides=4)(inputs)
+    layer1 = layers.BatchNormalization()(layer1)
+    layer1 = layers.Activation('relu')(layer1)
+
+    layer2 = layers.Conv2D(64, 4, strides=2)(layer1)
+    layer2 = layers.BatchNormalization()(layer2)
+    layer2 = layers.Activation('relu')(layer2)
+
+    layer3 = layers.Conv2D(64, 3, strides=1)(layer2)
+    layer3 = layers.BatchNormalization()(layer3)
+    layer3 = layers.Activation('relu')(layer3)
+
+    layer4 = layers.Flatten()(layer3)
+
+    layer5 = layers.Dense(output_dim=64)(layer4)
+    layer5 = layers.BatchNormalization()(layer5)
+    layer5 = layers.Activation('relu')(layer5)
+
+    layer6 = layers.Dense(output_dim=32)(layer5)
+    layer6 = layers.BatchNormalization()(layer6)
+    layer6 = layers.Activation('relu')(layer6)
+
+    action = layers.Dense(output_dim=action_space, activation='softmax')(layer6)
+    return keras.Model(inputs=inputs, outputs=action)
+
+def critic_network(input_shape, action_space):
+    inputs = layers.Input(shape=(input_shape))
+
+    layer1 = layers.Conv2D(32, 8, strides=4)(inputs)
+    layer1 = layers.BatchNormalization()(layer1)
+    layer1 = layers.Activation('relu')(layer1)
+
+    layer2 = layers.Conv2D(64, 4, strides=2)(layer1)
+    layer2 = layers.BatchNormalization()(layer2)
+    layer2 = layers.Activation('relu')(layer2)
+
+    layer3 = layers.Conv2D(64, 3, strides=1)(layer2)
+    layer3 = layers.BatchNormalization()(layer3)
+    layer3 = layers.Activation('relu')(layer3)
+
+    layer4 = layers.Flatten()(layer3)
+    
+    layer5 = layers.Dense(output_dim=64)(layer4)
+    layer5 = layers.BatchNormalization()(layer5)
+    layer5 = layers.Activation('relu')(layer5)
+
+    layer6 = layers.Dense(output_dim=32)(layer5)
+    layer6 = layers.BatchNormalization()(layer6)
+    layer6 = layers.Activation('relu')(layer6)
+
+    action = layers.Dense(output_dim=action_space, activation='linear')(layer6)
     return keras.Model(inputs=inputs, outputs=action)
