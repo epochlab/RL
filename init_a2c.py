f8be777df2726584d7983df5ecb26f89f932f115
James Roberts <JRoberts86@gmail.com>
2021-08-24 17:56:25 +0100

    Add capabilitiy for a2c

--- init_a2c.py
+++ init_a2c.py
@@ -4,6 +4,7 @@ import numpy as np
 import tensorflow as tf
 
 from wrappers.doom import Sandbox
+from networks import actor_network, critic_network
 from utils import load_config, log_feedback, save, load
 
 # -----------------------------
@@ -15,14 +16,18 @@ print("Eager mode:", tf.executing_eagerly())
 
 # -----------------------------
 
-config = load_config('config.yml')['doom']
+config = load_config('config.yml')['doom-a2c']
 log_dir = "metrics/"
 
 # -----------------------------
 
 sandbox = Sandbox(config)
 env, action_space = sandbox.build_env(config['env_name'])
+value_space = 1
 
-actor = actor_network(config['input_shape'], action_space)
-critic = critic_network(config['input_shape'], action_space)
+actor = actor_network(config['input_shape'], config['window_length'], action_space)
+critic = critic_network(config['input_shape'], config['window_length'], value_space)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=config['learning_rate'])
+
 actor.summary()
--- networks.py
+++ networks.py
@@ -3,6 +3,7 @@
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import backend as K
+from tensorflow.keras.optimizers import Adam
 
 # DQN -------------------------
 
@@ -17,7 +18,10 @@ def dqn(input_shape, window_length, action_space):
     layer5 = layers.Dense(512, activation="relu", kernel_initializer='he_uniform')(layer4)
 
     action = layers.Dense(action_space, activation="linear", kernel_initializer='he_uniform')(layer5)
-    return keras.Model(inputs=inputs, outputs=action)
+
+    model = keras.Model(inputs=inputs, outputs=action)
+    model.compile(loss='mse')
+    return model
 
 def dueling_dqn(input_shape, window_length, action_space):
     inputs = layers.Input(shape=(input_shape[0], input_shape[1], window_length,))
@@ -36,12 +40,15 @@ def dueling_dqn(input_shape, window_length, action_space):
     adv = layers.Lambda(lambda a: a[:, :] - K.mean(a[:, :], keepdims=True), output_shape=(action_space,))(adv)
 
     action = layers.Add()([value, adv])
-    return keras.Model(inputs=inputs, outputs=action)
+
+    model = keras.Model(inputs=inputs, outputs=action)
+    model.compile(loss='mse')
+    return model
 
 # A2C -------------------------
 
-def actor_network(input_shape, action_space):
-    inputs = layers.Input(shape=(input_shape))
+def actor_network(input_shape, window_length, action_space):
+    inputs = layers.Input(shape=(input_shape[0], input_shape[1], window_length,))
 
     layer1 = layers.Conv2D(32, 8, strides=4)(inputs)
     layer1 = layers.BatchNormalization()(layer1)
@@ -57,19 +64,22 @@ def actor_network(input_shape, action_space):
 
     layer4 = layers.Flatten()(layer3)
 
-    layer5 = layers.Dense(output_dim=64)(layer4)
+    layer5 = layers.Dense(64)(layer4)
     layer5 = layers.BatchNormalization()(layer5)
     layer5 = layers.Activation('relu')(layer5)
 
-    layer6 = layers.Dense(output_dim=32)(layer5)
+    layer6 = layers.Dense(32)(layer5)
     layer6 = layers.BatchNormalization()(layer6)
     layer6 = layers.Activation('relu')(layer6)
 
-    action = layers.Dense(output_dim=action_space, activation='softmax')(layer6)
-    return keras.Model(inputs=inputs, outputs=action)
+    action = layers.Dense(action_space, activation='softmax')(layer6)
 
-def critic_network(input_shape, action_space):
-    inputs = layers.Input(shape=(input_shape))
+    model = keras.Model(inputs=inputs, outputs=action)
+    model = model.compile(loss='categorical_crossentropy')
+    return model
+
+def critic_network(input_shape, window_length, value_space):
+    inputs = layers.Input(shape=(input_shape[0], input_shape[1], window_length,))
 
     layer1 = layers.Conv2D(32, 8, strides=4)(inputs)
     layer1 = layers.BatchNormalization()(layer1)
@@ -84,14 +94,17 @@ def critic_network(input_shape, action_space):
     layer3 = layers.Activation('relu')(layer3)
 
     layer4 = layers.Flatten()(layer3)
-    
-    layer5 = layers.Dense(output_dim=64)(layer4)
+
+    layer5 = layers.Dense(64)(layer4)
     layer5 = layers.BatchNormalization()(layer5)
     layer5 = layers.Activation('relu')(layer5)
 
-    layer6 = layers.Dense(output_dim=32)(layer5)
+    layer6 = layers.Dense(32)(layer5)
     layer6 = layers.BatchNormalization()(layer6)
     layer6 = layers.Activation('relu')(layer6)
 
-    action = layers.Dense(output_dim=action_space, activation='linear')(layer6)
-    return keras.Model(inputs=inputs, outputs=action)
+    action = layers.Dense(value_space, activation='linear')(layer6)
+
+    model = keras.Model(inputs=inputs, outputs=action)
+    model.compile(loss='mse')
+    return model
